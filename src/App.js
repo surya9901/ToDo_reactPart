cc0bedaf57708b7f96ef99884a23a3a391eb8d1d
surya9901 <surya9901@gmail.com>
2021-10-05 22:37:06 +0530

    React Part Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // this is used to store the data receiving from the server 
+  const [todo, setToDo] = useState([])
+
+  // this is used to show the loading screen onload
+  const [load, setLoad] = useState(true)
+
+  // this is used to get the data from the input box
+  const [task, setTask] = useState("")
+
+  // useEffect is used to render the data on load/Intializing of the page  
+  useEffect(async () => {
+    fetchTaskList()
+  }, [])
+
+  // Api call to get the data from the server
+  let fetchTaskList = async () => {
+    try {
+      // using axios package to fetch the data 
+      let todo_data = await axios.get("http://localhost:3000/list-all-todo")
+
+      // this is a hook used to store the data in the todo variable
+      setToDo([...todo_data.data])
+      setLoad(false)
+    }
+    catch (error) {
+      window.alert("Sorry something went wrong!")
+      setLoad(false)
+    }
+  }
+
+  // Api call to post the data into the server
+  let handlecreate = async () => {
+    try {
+      let postData = await axios.post("http://localhost:3000/create-task", { message: task })
+
+      // calling the get method to display the data in DOM
+      fetchTaskList()
+
+      // making the input box empty after 
+      // the data is pushed to the server and displayed on the DOM
+      setTask("")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  let handleChange = async (e, id) => {
+    try {
+      let updateData = await axios.put(`http://localhost:3000/update-task/${id}`, { status: e.target.checked })
+      fetchTaskList()
+    }
+    catch (error) {
+      alert(error);
+    }
+  }
+
+  let handleDelete = async (id) => {
+    try {
+      // alert(id)
+      let deleteData = await axios.delete(`http://localhost:3000/delete-task/${id}`)
+      fetchTaskList()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="row mt-2">
+        <h4>Add Task:</h4>
+        <div className="input_div col-lg-8">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" value={task} onChange={(e) => setTask(e.target.value)} placeholder="Type Here..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <button onClick={() => handlecreate()} className="btn btn-outline-secondary" type="button" id="button-addon2">Submit</button>
+          </div>
+        </div>
+        <div className="col-lg-8">
+          <ul className="list-group">
+            {
+              load ? <h1>Loading...</h1> :
+                todo.map((data) => {
+                  return (
+                    <li className="taskDisplay_div list-group-item">
+                      <div className="liTaskDisplay">
+                        <input className="form-check-input me-1" checked={data.status} type="checkbox" value="" aria-label="..." onChange={(e) => handleChange(e, data.id)} />
+                        <span style={{ textDecoration: data.status ? "line-through" : "" }}>{data.message}</span>
+                      </div>
+                      <div className="deleteButton_div">
+                        <span className="badge bg-danger rounded-pill" onClick={() => handleDelete(data.id)}>X</span>
+                      </div>
+                    </li>
+                  )
+                })
+            }
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
